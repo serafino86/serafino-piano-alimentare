34c95ed0e024050f5491aa52297dde1e203ae35f
serafino86 <serafino86@users.noreply.github.com>
2026-03-24 00:27:57 +0100

    Apri direttamente il giorno di oggi all'avvio

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation';
 
 export default function Home() {
   const now = new Date();
-  redirect(`/${now.getFullYear()}/${now.getMonth() + 1}`);
+  redirect(`/${now.getFullYear()}/${now.getMonth() + 1}/${now.getDate()}`);
 }
